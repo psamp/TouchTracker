51b8f7860d939e7f0688a9af4ef631d0b4fbbf44
Princess Sampson <ps@kingonomics.com>
2016-09-18 12:52:07 -0400

    Initial Commit

---
+++ TouchTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchTracker
+//
+//  Created by Princess Sampson on 9/18/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
